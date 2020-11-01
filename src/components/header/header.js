f5569f108bb201077d12b4f769eb468c16496bb1
Pavel Dreit <pavel@dreit.ru>
2020-11-01 15:04:51 +0500

    Add header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,3 +1,17 @@
 (()=>{
+	var mainHeader = document.querySelector('header.main-header');
+	if (!mainHeader) return false;
 
+	function updateHeaderSticking() {
+		var scrolled = window.pageYOffset || document.documentElement.scrollTop;
+		mainHeader.classList.toggle('unsticked', scrolled > 5);
+	}
+
+	window.addEventListener('scroll', function() {
+		window.throttling(updateHeaderSticking)
+	}, false);
+
+	window.addEventListener('load', function () {
+		updateHeaderSticking();
+	}, false);
 })()
