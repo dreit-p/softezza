c65b8cf4c1069da14ea7fa31ee9726f02bfd8c6e
Pavel Dreit <pavel@dreit.ru>
2020-11-12 16:25:19 +0500

    Add notifications

--- src/main.js
+++ src/main.js
@@ -9,4 +9,6 @@ import './components/header/header.js';
 import './components/modals/modals.js';
 import './components/modals/contents/feedback.js';
 
+import './components/notifications/notifications.js';
+
 /*=====  End of components  ======*/
